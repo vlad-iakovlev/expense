999444261d2edcc54572be31f0b946d0ad43b25a
Vladislav Yakovlev <vlad309523@gmail.com>
2023-01-31 04:58:22 +0400

    fix: fix formatAmount

--- utils/formatAmount.ts
+++ utils/formatAmount.ts
@@ -1,5 +1,5 @@
 import { ClientCurrency } from '../api/types/currencies'
 
 export const formatAmount = (amount: number, currency: ClientCurrency) => {
-  return `${(amount / 1e4).toFixed(2)}${currency.symbol}`
+  return `${currency.symbol}${(amount / 1e4).toFixed(2)}`
 }
