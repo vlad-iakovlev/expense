945294d3066816a53f009d3055ab99324284ca42
Vladislav Yakovlev <vlad309523@gmail.com>
2023-02-06 04:26:44 +0400

    feat: update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,6 +23,7 @@ export const MyDocument: FC<DocumentProps> = () => {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#16a34a" />
       </Head>
 
       <body className="bg-zinc-300">
