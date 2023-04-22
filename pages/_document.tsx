ee5a5887b42571701e5a59cae668882c4d173fd2
Vladislav Yakovlev <vlad309523@gmail.com>
2023-04-23 01:32:08 +0400

    feat: add service worker

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,24 +5,13 @@ export const MyDocument: FC<DocumentProps> = () => {
   return (
     <Html>
       <Head>
+        <link rel="icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/manifest.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#16a34a" />
       </Head>
 
