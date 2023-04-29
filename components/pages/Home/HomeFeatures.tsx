8bbfc25bca119f0fa8e661f02fc9c1c1326ce4af
Vladislav Yakovlev <vlad309523@gmail.com>
2023-04-30 03:56:10 +0400

    fix: update landing

--- components/pages/Home/HomeFeatures.tsx
+++ components/pages/Home/HomeFeatures.tsx
@@ -2,9 +2,9 @@ import {
   ArrowPathIcon,
   ChartPieIcon,
   CurrencyDollarIcon,
-  RectangleGroupIcon,
-  UserGroupIcon,
-  WifiIcon,
+  SignalSlashIcon,
+  TagIcon,
+  UsersIcon,
 } from '@heroicons/react/24/outline'
 import { FC } from 'react'
 import { Card } from '../../ui-kit/Card/Card.tsx'
@@ -17,23 +17,23 @@ export const HomeFeatures: FC<Props> = ({ className }) => (
   <Card className={className}>
     <Card.Title title="Features" />
     <Card.Divider />
-    <Card.Text start={<WifiIcon className="w-6 h-6" />}>
-      Fully online expense tracker
-    </Card.Text>
     <Card.Text start={<ArrowPathIcon className="w-6 h-6" />}>
-      Built-in sync between devices
+      Built-in cross-device sync
     </Card.Text>
-    <Card.Text start={<UserGroupIcon className="w-6 h-6" />}>
-      Manage expenses together
+    <Card.Text start={<SignalSlashIcon className="w-6 h-6" />}>
+      Available even offline
     </Card.Text>
-    <Card.Text start={<RectangleGroupIcon className="w-6 h-6" />}>
-      Categorize your expenses
+    <Card.Text start={<TagIcon className="w-6 h-6" />}>
+      Flexible categorization
     </Card.Text>
-    <Card.Text start={<ChartPieIcon className="w-6 h-6" />}>
-      Pie chart for week/month/year
+    <Card.Text start={<UsersIcon className="w-6 h-6" />}>
+      Collaborative tracking
     </Card.Text>
     <Card.Text start={<CurrencyDollarIcon className="w-6 h-6" />}>
       Automatic currency conversion
+    </Card.Text>
+    <Card.Text start={<ChartPieIcon className="w-6 h-6" />}>
+      Pie charts for week/month/year
     </Card.Text>
   </Card>
 )
