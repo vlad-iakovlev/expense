336b31421aece996245e3259528502792916f1e7
Vladislav Yakovlev <vlad309523@gmail.com>
2023-04-29 19:47:28 +0400

    fix: do not reload page when back online

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 import withPWA from 'next-pwa'
 
-const nextConfig = withPWA({ dest: 'public' })()
+const nextConfig = withPWA({
+  dest: 'public',
+  reloadOnOnline: false,
+})()
 
 export default nextConfig
