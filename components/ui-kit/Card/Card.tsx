26b36765390e59ba5b36047bf9ba643d0c4ed07a
Vladislav Yakovlev <vlad309523@gmail.com>
2023-04-30 18:14:05 +0400

    fix: make cards on mobile wider

--- components/ui-kit/Card/Card.tsx
+++ components/ui-kit/Card/Card.tsx
@@ -31,7 +31,7 @@ export const Card = ({ className, children }: CardProps) => {
     <div
       className={clsx(
         className,
-        'py-2 bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5'
+        'max-sm:-mx-4 py-2 bg-white sm:rounded-md shadow-lg ring-1 ring-black ring-opacity-5'
       )}
     >
       {children}
