69268364e3d43ee1c613947e89d1688d62480ec6
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-02 02:23:20 +0400

    fix: disable scale

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,7 +23,7 @@ const App: FC<AppProps<{ session: Session | null }>> = ({
       <NextHead>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no"
         />
       </NextHead>
 
