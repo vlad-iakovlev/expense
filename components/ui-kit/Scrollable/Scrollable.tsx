379b696c766a5679529c82eb2b58259a0055dec5
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-03 17:04:54 +0400

    refactor: simplify scrollable

--- components/ui-kit/Scrollable/Scrollable.tsx
+++ components/ui-kit/Scrollable/Scrollable.tsx
@@ -1,5 +1,5 @@
 import { clsx } from 'clsx'
-import { AnimatePresence, HTMLMotionProps, motion } from 'framer-motion'
+import { AnimatePresence, Variants, motion } from 'framer-motion'
 import { FC, ReactNode, useEffect, useMemo, useRef, useState } from 'react'
 import { Thumb, Track, getThumb, getTrack } from './utils.ts'
 
@@ -12,6 +12,18 @@ const DEFAULT_TRACK: Track = {
 }
 
 const DEFAULT_THUMB: Thumb = { offset: 0, length: 0 }
+
+const variants: Variants = {
+  open: {
+    opacity: 1,
+    transition: { ease: 'easeOut', duration: 0.075 },
+  },
+
+  closed: {
+    opacity: 0,
+    transition: { ease: 'easeIn', duration: 0.2 },
+  },
+}
 
 export interface ScrollableProps {
   className?: string
@@ -89,34 +101,28 @@ export const Scrollable: FC<ScrollableProps> = ({
     }
   }, [isVTrackVisible, isHTrackVisible, vTrack, hTrack, vThumb, hThumb])
 
-  const tracks = useMemo<ReactNode[]>(() => {
-    const trackProps: HTMLMotionProps<'div'> = {
-      className: 'absolute pointer-events-none',
-      initial: {
-        opacity: 0,
-      },
-      animate: {
-        opacity: 1,
-        transition: { ease: 'easeOut', duration: 0.075 },
-      },
-      exit: {
-        opacity: 0,
-        transition: { ease: 'easeIn', duration: 0.2 },
-      },
-    }
-
-    const thumbClassName = clsx('absolute rounded-full', {
+  const thumbClassName = useMemo(() => {
+    return clsx('absolute rounded-full', {
       'bg-black bg-opacity-[0.35]': theme === 'light',
       'bg-white bg-opacity-50': theme === 'dark',
     })
+  }, [theme])
 
-    const tracks: ReactNode[] = []
+  return (
+    <div className={clsx(className, 'relative overflow-hidden')}>
+      <div
+        ref={contentRef}
+        className={clsx(contentClassName, 'overflow-auto hide-scrollbars')}
+      >
+        {children}
+      </div>
 
-    if (isVTrackVisible) {
-      tracks.push(
+      <AnimatePresence>
         <motion.div
-          {...trackProps}
           key="v-track"
+          className="absolute pointer-events-none"
+          animate={isVTrackVisible ? 'open' : 'closed'}
+          variants={variants}
           style={{
             top: vTrack.startOffset,
             bottom: vTrack.endOffset,
@@ -129,14 +135,12 @@ export const Scrollable: FC<ScrollableProps> = ({
             style={{ top: vThumb.offset, height: vThumb.length }}
           />
         </motion.div>
-      )
-    }
 
-    if (isHTrackVisible) {
-      tracks.push(
         <motion.div
-          {...trackProps}
           key="h-track"
+          className="absolute pointer-events-none"
+          animate={isHTrackVisible ? 'open' : 'closed'}
+          variants={variants}
           style={{
             left: hTrack.startOffset,
             right: hTrack.endOffset,
@@ -149,22 +153,7 @@ export const Scrollable: FC<ScrollableProps> = ({
             style={{ left: hThumb.offset, width: hThumb.length }}
           />
         </motion.div>
-      )
-    }
-
-    return tracks
-  }, [hThumb, hTrack, isHTrackVisible, isVTrackVisible, theme, vThumb, vTrack])
-
-  return (
-    <div className={clsx(className, 'relative overflow-hidden')}>
-      <div
-        ref={contentRef}
-        className={clsx(contentClassName, 'overflow-auto hide-scrollbars')}
-      >
-        {children}
-      </div>
-
-      <AnimatePresence>{tracks}</AnimatePresence>
+      </AnimatePresence>
     </div>
   )
 }
