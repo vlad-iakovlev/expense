9fd8ea5a14a2e39c9e8f179b990fe92e8c1d6e8d
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-04 05:15:25 +0400

    revert: "fix: title font-size"
    This reverts commit a289a1f5edacc940252980e2a8e4d746498d892d.

--- components/ui-kit/Title/Title.tsx
+++ components/ui-kit/Title/Title.tsx
@@ -5,5 +5,5 @@ export interface TitleProps {
 }
 
 export const Title: FC<TitleProps> = ({ title }) => {
-  return <h1 className="my-8 text-3xl font-bold truncate">{title}</h1>
+  return <h1 className="my-8 text-4xl font-bold truncate">{title}</h1>
 }
