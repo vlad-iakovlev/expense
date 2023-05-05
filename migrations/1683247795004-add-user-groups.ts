878b0efe98493b69f9900d8e92460d4c04ab1cc0
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-05 05:05:24 +0400

    feat: add user-group collection

---
+++ migrations/1683247795004-add-user-groups.ts
@@ -0,0 +1,22 @@
+import { Db } from 'mongodb'
+
+export const apply = async (db: Db) => {
+  const groups = await db.collection('Group').find({}).toArray()
+
+  const userGroups = groups
+    .map((group) =>
+      (group.userIds as string[]).map((userId) => ({
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        removed: group.removed as boolean,
+        userId,
+        groupId: group._id,
+      }))
+    )
+    .flat()
+
+  await db.collection('UserGroup').insertMany(userGroups)
+
+  await db.collection('User').updateMany({}, { $unset: { groupId: '' } })
+  await db.collection('Group').updateMany({}, { $unset: { userIds: '' } })
+}
