a98efc044638cf4a39bc0d508fdafecd0b3bec33
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-09 01:32:11 +0400

    fix: update landing

--- components/pages/Home.tsx
+++ components/pages/Home.tsx
@@ -73,7 +73,7 @@ export const Home: FC = () => {
           />
           <Card.Text
             prefix={<UsersIcon className="w-6 h-6" />}
-            label="Collaborative tracking (WIP)"
+            label="Collaborative tracking"
           />
           <Card.Text
             prefix={<CurrencyDollarIcon className="w-6 h-6" />}
