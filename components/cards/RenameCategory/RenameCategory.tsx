5a005dfefe67643a9598104c0faa15751daa0485
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-08 23:04:21 +0400

    feat: add apply button to category rename

--- components/cards/RenameCategory/RenameCategory.tsx
+++ components/cards/RenameCategory/RenameCategory.tsx
@@ -1,3 +1,4 @@
+import { CheckIcon } from '@heroicons/react/24/outline'
 import { FC, useCallback, useMemo, useState } from 'react'
 import { useCategories } from '../../../stores/RootStore/hooks/useCategories.ts'
 import { Card, CardSelectOption } from '../../ui-kit/Card/Card.tsx'
@@ -10,6 +11,7 @@ interface Props {
 export const RenameCategoryCard: FC<Props> = ({ className, groupId }) => {
   const { categories, renameCategory } = useCategories({ groupId })
   const [category, setCategory] = useState('')
+  const [newName, setNewName] = useState('')
 
   const categoriesOptions = useMemo<CardSelectOption[]>(() => {
     return categories.map((category) => ({
@@ -28,15 +30,17 @@ export const RenameCategoryCard: FC<Props> = ({ className, groupId }) => {
 
   const handleSelectCategory = useCallback((option: CardSelectOption) => {
     setCategory(option.id)
+    setNewName(option.id)
   }, [])
 
-  const handleNameChange = useCallback(
-    (name: string) => {
-      renameCategory(category, name)
-      setCategory('')
-    },
-    [category, renameCategory]
-  )
+  const handleApplyClick = useCallback(() => {
+    if (category !== newName) {
+      renameCategory(category, newName)
+    }
+
+    setCategory('')
+    setNewName('')
+  }, [category, newName, renameCategory])
 
   if (!categories.length) {
     return null
@@ -55,11 +59,15 @@ export const RenameCategoryCard: FC<Props> = ({ className, groupId }) => {
       />
 
       {category ? (
-        <Card.Input
-          label="New name"
-          value={category}
-          onChange={handleNameChange}
-        />
+        <>
+          <Card.Input label="New name" value={newName} onChange={setNewName} />
+          <Card.Divider />
+          <Card.Button
+            prefix={<CheckIcon className="w-6 h-6" />}
+            label="Apply"
+            onClick={handleApplyClick}
+          />
+        </>
       ) : null}
     </Card>
   )
