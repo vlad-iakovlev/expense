404bf2d327854b2ee49696f9a165393243a8ffe8
Vladislav Yakovlev <vlad309523@gmail.com>
2023-05-13 14:21:02 +0400

    fix: apply migration before setting indexes

--- migrations/1683972626966-rename-currency-name-to-symbol.ts
+++ migrations/1683972626966-rename-currency-name-to-symbol.ts
@@ -1,6 +1,8 @@
 import { Db } from 'mongodb'
 
 export const apply = async (db: Db) => {
+  await db.collection('Currency').dropIndexes()
+
   await db
     .collection('Currency')
     .updateMany({}, { $rename: { name: 'symbol' } })
