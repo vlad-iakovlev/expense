ac9f740a2a8662971609c263746af92a80855a12
Vladislav Yakovlev <vlad309523@gmail.com>
2023-07-27 14:56:30 +0400

    fix: breadcrums accessibility

--- components/ui-kit/Breadcrumbs/Breadcrumbs.tsx
+++ components/ui-kit/Breadcrumbs/Breadcrumbs.tsx
@@ -19,20 +19,27 @@ export const Breadcrumbs = ({
   return (
     <>
       {!!parents?.length && (
-        <div className="max-md:hidden flex items-center gap-2 -mt-2 -mb-4">
+        <nav
+          className="max-md:hidden flex items-center gap-2 -mt-2 -mb-4"
+          aria-label="Breadcrumbs"
+        >
           {parents.map((parent) => (
             <Fragment key={parent.href}>
               <NextLink
                 className="min-w-0 text-lg leading-6 font-medium text-cyan-900 truncate"
                 href={{ pathname: parent.href, query: { animation: 'back' } }}
                 as={parent.href}
+                aria-label={`Go back to ${parent.title}`}
               >
                 {parent.title}
               </NextLink>
-              <ChevronRightIcon className="flex-none w-5 h-5" />
+              <ChevronRightIcon
+                className="flex-none w-5 h-5"
+                aria-hidden="true"
+              />
             </Fragment>
           ))}
-        </div>
+        </nav>
       )}
 
       {mobileBack ? (
@@ -40,6 +47,7 @@ export const Breadcrumbs = ({
           className="md:hidden flex items-center gap-2 -mt-2 -mb-4 text-lg leading-6 font-medium text-cyan-900 truncate"
           href={{ pathname: mobileBack.href, query: { animation: 'back' } }}
           as={mobileBack.href}
+          aria-label={`Go back to ${mobileBack.title}`}
         >
           <ArrowUturnLeftIcon className="flex-none w-5 h-5" />
           {mobileBack.title}
