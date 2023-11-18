ce1dcd1f1c59dfc5a9916c1746a82202ad6dcd9d
Vladislav Yakovlev <vlad309523@gmail.com>
2023-11-18 18:36:33 +0400

    chore: update next-pwa

--- next.config.js
+++ next.config.js
@@ -1,36 +1,44 @@
-import nextPWA from 'next-pwa'
-import nextPWACache from 'next-pwa/cache.js'
+// @ts-check
+import nextPWA from '@ducanh2912/next-pwa'
 
 const withPWA = nextPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   reloadOnOnline: false,
-  runtimeCaching: [
-    {
-      urlPattern: ({ url }) => {
-        const isSameOrigin = self.origin === url.origin
-        if (!isSameOrigin) return false
-        const pathname = url.pathname
-        // Exclude /api/auth/callback/* to fix OAuth workflow in Safari without impact other environment
-        // Above route is default for next-auth, you may need to change it if your OAuth workflow has a different callback route
-        // Issue: https://github.com/shadowwalker/next-pwa/issues/131#issuecomment-821894809
-        if (pathname.startsWith('/api/auth/callback/')) return false // <= THIS LINE IS PATCHED
-        if (pathname.startsWith('/api/')) return true
-        return false
-      },
-      handler: 'NetworkFirst',
-      method: 'GET',
-      options: {
-        cacheName: 'apis',
-        expiration: {
-          maxEntries: 16,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
+
+  // Fix next-auth when offline
+  // TODO: Add link to PR in @ducanh2912/next-pwa
+  extendDefaultRuntimeCaching: true,
+  workboxOptions: {
+    runtimeCaching: [
+      {
+        urlPattern: ({ sameOrigin, url: { pathname } }) => {
+          // Exclude /api/auth/callback/* to fix OAuth workflow in Safari without having an impact on other environments
+          // The above route is the default for next-auth, you may need to change it if your OAuth workflow has a different callback route
+          // Issue: https://github.com/shadowwalker/next-pwa/issues/131#issuecomment-821894809
+          if (!sameOrigin || pathname.startsWith('/api/auth/callback/')) {
+            return false
+          }
+
+          if (pathname.startsWith('/api/')) {
+            return true
+          }
+
+          return false
         },
-        networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
+        handler: 'NetworkFirst',
+        method: 'GET',
+        options: {
+          cacheName: 'apis',
+          expiration: {
+            maxEntries: 16,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+          networkTimeoutSeconds: 10, // fallback to cache if API does not response within 10 seconds
+        },
       },
-    },
-    ...nextPWACache,
-  ],
+    ],
+  },
 })
 
 const nextConfig = {
