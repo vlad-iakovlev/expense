e1b74f9162f6ec0c2462c81d4adf3143aa182541
Vladislav Yakovlev <vlad309523@gmail.com>
2023-12-17 19:33:48 +0400

    fix: currency badge text color

--- components/ui-kit/CurrencyBadge/CurrencyBadge.tsx
+++ components/ui-kit/CurrencyBadge/CurrencyBadge.tsx
@@ -7,7 +7,7 @@ export interface CurrencyBadgeProps {
 export const CurrencyBadge = ({ currency }: CurrencyBadgeProps) => {
   return (
     <div
-      className="text-tertiary bg-quaternary flex w-14 flex-none items-center justify-center rounded-md px-2 py-1 text-sm"
+      className="text-secondary bg-quaternary flex w-14 flex-none items-center justify-center rounded-md px-2 py-1 text-sm"
       aria-hidden="true"
     >
       {currency.symbol}
