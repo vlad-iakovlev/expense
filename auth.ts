239235d4abb0f62eeb10ec7fdaaee2f8afccfe2b
Vladislav Yakovlev <vlad309523@gmail.com>
2024-04-27 04:44:32 +0400

    fix: typo

--- auth.ts
+++ auth.ts
@@ -9,7 +9,7 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks: {
     session({ session, user }) {
       return produce(session, (draft) => {
-        if (session.user) {
+        if (draft.user) {
           draft.user.id = user.id
         }
       })
