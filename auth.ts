bdb6010408a98a12ac5ce5860509e18582cc57d7
Vladislav Yakovlev <vlad309523@gmail.com>
2024-04-27 04:43:54 +0400

    fix: user id in client session

--- auth.ts
+++ auth.ts
@@ -1,9 +1,19 @@
 import { PrismaAdapter } from '@auth/prisma-adapter'
+import { produce } from 'immer'
 import NextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
 import { prisma } from './utils/server/prisma.js'
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session({ session, user }) {
+      return produce(session, (draft) => {
+        if (session.user) {
+          draft.user.id = user.id
+        }
+      })
+    },
+  },
   providers: [Google],
 })
