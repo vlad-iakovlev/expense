53e2392dfcd1be785294633ad6c16f3deb3916b8
Vladislav Yakovlev <vlad309523@gmail.com>
2026-03-30 05:26:55 +0200

    fix: build in Docker

--- utils/server/prisma.ts
+++ utils/server/prisma.ts
@@ -1,10 +1,8 @@
 import { PrismaPostgresAdapter } from '@prisma/adapter-ppg'
-import assert from 'assert'
 import { PrismaClient } from '@/generated/prisma/client'
 
-const connectionString = process.env.DATABASE_URL
-assert(connectionString, 'DATABASE_URL is not set')
-
-const adapter = new PrismaPostgresAdapter({ connectionString })
+const adapter = new PrismaPostgresAdapter({
+  connectionString: process.env.DATABASE_URL ?? '',
+})
 
 export const prisma = new PrismaClient({ adapter })
