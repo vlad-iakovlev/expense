0ada5ad3ca268e523853ffe235c97368aa62dbc9
Vladislav Yakovlev <vlad309523@gmail.com>
2026-03-30 04:52:51 +0200

    fix: better auth envs

--- auth.ts
+++ auth.ts
@@ -1,27 +1,20 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { prismaAdapter } from '@better-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
-import assert from 'assert'
 import { betterAuth } from 'better-auth'
-
-assert(process.env.AUTH_URL, 'AUTH_URL is not set')
-assert(process.env.AUTH_SECRET, 'AUTH_SECRET is not set')
-assert(process.env.AUTH_GOOGLE_ID, 'AUTH_GOOGLE_ID is not set')
-assert(process.env.AUTH_GOOGLE_SECRET, 'AUTH_GOOGLE_SECRET is not set')
 
 const prisma = new PrismaClient()
 
 export const auth = betterAuth({
   appName: 'Expense',
-  baseURL: process.env.AUTH_URL,
-  secret: process.env.AUTH_SECRET,
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
     transaction: true,
   }),
   socialProviders: {
     google: {
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       overrideUserInfoOnSignIn: true,
     },
   },
