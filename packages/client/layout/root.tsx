e6e1e546998f5424d5c74c21ca0b163bb792ab2c
Vladislav Yakovlev <vlad309523@gmail.com>
2026-04-01 03:39:03 +0200

    chore: enable compile

--- packages/client/layout/root.tsx
+++ packages/client/layout/root.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from '@/components/App.js'
+import { App } from '@/components/App'
 import '@/styles/globals.css'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
