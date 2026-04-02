72678480dd457876a079bb347997b4463cf76680
Vladislav Yakovlev <vlad309523@gmail.com>
2026-04-02 12:20:20 +0200

    chore: codestyle

--- packages/server/routes/auth/index.ts
+++ packages/server/routes/auth/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { AuthType, auth } from '@/auth.js'
 
 const router = new Hono<{ Bindings: AuthType }>({ strict: false })
+
 router.on(['POST', 'GET'], '*', (c) => auth.handler(c.req.raw))
 
 export default router
