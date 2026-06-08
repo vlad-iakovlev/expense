8225133c1900d5a99517fed98162b883038f4b17
Vladislav Iakovlev <vlad309523@gmail.com>
2026-06-09 01:57:42 +0200

    chore: add file variables support

--- docker/backend-entrypoint.sh
+++ docker/backend-entrypoint.sh
@@ -1,6 +1,33 @@
 #!/bin/sh
 set -e
 
+# Source: https://github.com/tiredofit/docker-alpine/blob/06ce99a4f2ddde26a0185c5782d78a057f16bd35/install/assets/functions/00-container#L1446-L1465
+transform_file_var() {
+  local variables
+  variables=$(echo "$@" | tr " " "\n")
+  for variable in $variables; do
+    if [ -v "${variable}"_FILE ] ; then
+      file_variable=${variable}_FILE
+      if [ ! -f "${!file_variable}" ] ; then
+        print_error "[transform_file_var] ${variable}_FILE set as environment variable, however file doesn't exist"
+        return 1
+      fi
+      export "${variable}"="$(cat "${!file_variable}")"
+    fi
+  done
+  unset file_variable
+  unset variables
+}
+
+transform_file_var \
+  DATABASE_URL \
+  CRON_SECRET \
+  BETTER_AUTH_URL \
+  BETTER_AUTH_SECRET \
+  GOOGLE_CLIENT_ID \
+  GOOGLE_CLIENT_SECRET \
+  EXCHANGE_RATES_API_KEY
+
 echo "Applying database migrations..."
 npm run prisma -- migrate deploy
 
